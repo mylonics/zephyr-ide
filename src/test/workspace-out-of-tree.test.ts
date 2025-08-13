d6be59131c379c06de2b9bf700b645674cd96d6c
Rijesh Augustine <rijeshaugustine@gmail.com>
2025-08-13 11:55:10 -0600

    Another attempt at fixing test

--- src/test/workspace-out-of-tree.test.ts
+++ src/test/workspace-out-of-tree.test.ts
@@ -123,8 +123,8 @@ suite("Workspace Out Of Tree Test Suite", () => {
             // Prime the mock interface for git setup with no_west branch
             uiMock.primeInteractions([
                 { type: 'input', value: '--branch no_west -- https://github.com/mylonics/zephyr-ide-samples.git', description: 'Enter git clone string for no_west branch' },
-                { type: 'quickpick', value: 'use-external-installation', description: 'Choose Use Existing Zephyr Installation option' },
-                { type: 'quickpick', value: 'global-install', description: 'Choose Global Installation option' },
+                { type: 'quickpick', value: '$(link) Use external Zephyr installation', description: 'Choose Use Existing Zephyr Installation option' },
+                { type: 'quickpick', value: '$(link) Global Installation', description: 'Choose Global Installation option' },
                 { type: 'quickpick', value: 'minimal', description: 'Select minimal manifest' },
                 { type: 'quickpick', value: 'stm32', description: 'Select STM32 toolchain' },
                 { type: 'quickpick', value: 'v4.2.0', description: 'Select default configuration' },
