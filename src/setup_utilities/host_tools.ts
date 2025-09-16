1c369224fcc676586b582e3fc81905ad8713b1b0
Rijesh Augustine <rijeshaugustine@gmail.com>
2025-09-16 10:23:58 -0600

    Removed setx PATH from windows install instructions

--- src/setup_utilities/host_tools.ts
+++ src/setup_utilities/host_tools.ts
@@ -40,7 +40,7 @@ export async function installWindowsHostTools(context: vscode.ExtensionContext, 
   //Step 2
   // If winget is installed then we can install the required dependencies with this one liner;
 
-  let result = await executeTaskHelper("Install Host Tools", "winget install Kitware.CMake Ninja-build.Ninja oss-winget.gperf python Git.Git oss-winget.dtc wget 7zip.7zip; setx path '%path%;C:\\Program Files\\7-Zip'", "");
+  let result = await executeTaskHelper("Install Host Tools", "winget install Kitware.CMake Ninja-build.Ninja oss-winget.gperf python Git.Git oss-winget.dtc wget 7zip.7zip; [System.Environment]::SetEnvironmentVariable('Path', [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';C:\\Program Files\\7-Zip', 'Machine')", "");
 
   return result;
 }
